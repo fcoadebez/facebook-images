b37752c21de5b2e931608629cf6d15084bd7bb02
Clément Ollivier <clement.o2p@gmail.com>
2017-12-08 09:35:42 +0100

    :construction: Work on API

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from bson.json_util import dumps
 from FacebookScraper import FacebookScraper
 
+mongoClient = MongoClient()
+db = mongoClient.fbscrape
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
 @app.route('/scrape/<page>/<folder>')
 def scrape(page, folder):
-    fbUrl = 'https://www.facebook.com/pg/' + page
+    fbUrl = "https://www.facebook.com/pg/" + page
     myScraper = FacebookScraper(fbUrl, folder)
     myScraper.scrape()
 
-    return 'Les photos de la page ' + fbUrl + ' ont été téléchargées dans le dossier ' + folder
+    return "Les photos de la page " + fbUrl + " ont été téléchargées dans le dossier " + folder
+
+
+@app.route('/results')
+def results():
+    dbResults = dumps(db.fb_images.find())
+
+    return dbResults
